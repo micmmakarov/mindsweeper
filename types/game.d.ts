31b6eebc3659e5112c1ed2ae7463049a964c4c18
Miša Mаkaröv <info1117@gmail.com>
2023-03-06 15:08:21 -0500

    Bumps version

--- types/game.d.ts
+++ types/game.d.ts
@@ -1,35 +1,35 @@
 export interface Cell {
-  opened: boolean
-  hasMine: boolean
-  marked: boolean
+    opened: boolean;
+    hasMine: boolean;
+    marked: boolean;
 }
 export interface Size {
-  height: number
-  width: number
+    height: number;
+    width: number;
 }
 export interface Options {
-  size: Size
-  mines: number
+    size: Size;
+    mines: number;
 }
 interface Visited {
-  [key: string]: boolean
+    [key: string]: boolean;
 }
-type Col = Cell[]
-type Field = Col[]
+type Col = Cell[];
+type Field = Col[];
 interface Coordinate {
-  x: number
-  y: number
+    x: number;
+    y: number;
 }
 export declare class Game {
-  field: Field
-  size: Size
-  mines: number
-  lost: boolean
-  constructor(options?: Options)
-  generate(): void
-  setMines(): void
-  getCell(coordinate: Coordinate): Cell
-  traverse(coordinate: Coordinate, visited?: Visited): void
-  open(coordinate: Coordinate): void
+    field: Field;
+    size: Size;
+    mines: number;
+    lost: boolean;
+    constructor(options?: Options);
+    generate(): void;
+    setMines(): void;
+    getCell(coordinate: Coordinate): Cell;
+    traverse(coordinate: Coordinate, visited?: Visited): void;
+    open(coordinate: Coordinate): void;
 }
-export {}
+export {};
